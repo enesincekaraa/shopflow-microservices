f979fd6a8ec0bda92caa8ab8ad48b1c6f539b4f2
Enes Incekara <enesincekara61@gmail.com>
2026-04-21 18:32:31 +0300

    feat: add Eureka Service Discovery

--- order-service/src/main/java/com/shopflow/orderservice/client/ProductClient.java
+++ order-service/src/main/java/com/shopflow/orderservice/client/ProductClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
-@FeignClient(name = "product-service", url = "${services.product-service.url}")
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
     @GetMapping("/api/products/{id}")
